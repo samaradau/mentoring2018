e7c7c205609226ea9bdd45c0a0a83457c0a71fb3
Yury Samaradau <Yury_Samaradau@epam.com>
2018-10-07 13:48:14 +0300

    Task 3 added

---
+++ Introduction to .NET/StandartClassLibrary/StandartClassLibrary/DemoClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StandartClassLibrary
+{
+    public class DemoClass
+    {
+		public string GetString(string name)
+		{
+			return $"{DateTime.UtcNow.ToString()} Hello, {name}!";
+		}
+    }
+}
