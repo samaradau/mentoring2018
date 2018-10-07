07a77bdd58079b6d53a9f9f9bfbd8bd5539fc111
samaradau <samorad1998@yandex.ru>
2018-10-07 13:56:59 +0300

    Added mono project

---
+++ Introduction to .NET/MonoConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CoreConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DemoClass demoClass = new DemoClass();
+            var name = Console.ReadLine();
+            Console.WriteLine(demoClass.GetString(name));
+            Console.ReadLine();
+        }
+    }
+}
