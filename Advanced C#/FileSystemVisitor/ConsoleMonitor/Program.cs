ee1eb4c1e25f659694f3a57fd4995690be4c99b3
Yury Samaradau <Yury_Samaradau@epam.com>
2018-10-07 20:38:25 +0300

    Task 2 added

---
+++ Advanced C#/FileSystemVisitor/ConsoleMonitor/Program.cs
@@ -0,0 +1,34 @@
+﻿using FileVisitor;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMonitor
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			FileSystemVisitor visitor = new FileSystemVisitor();
+			string[] files = Array.Empty<string>();
+			string[] dirs = Array.Empty<string>();
+
+			visitor.GetAllDirectoryElements(@"C:\Users\Yury_Samaradau\Desktop\mentoring2018\Advanced C#", ref dirs, ref files);
+
+			Console.WriteLine("\nDirectories: \n");
+
+			foreach (var el in dirs)
+				Console.WriteLine(el);
+
+			Console.WriteLine("\nFiles: \n");
+
+			foreach (var el in files)
+				Console.WriteLine(el);
+
+			Console.ReadKey();
+		}
+	}
+}
---
+++ Advanced C#/FileSystemVisitor/FileSystemVisitor/FileSystemVisitor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileVisitor
+{
+	public class FileSystemVisitor
+	{
+		public void GetAllDirectoryElements(string path, ref string[] directories, ref string[] files)
+		{
+			string[] _dirs = Array.Empty<string>();
+			string[] _files = Array.Empty<string>();
+
+			if (path != null)
+			{
+				_dirs = Directory.GetDirectories(path);
+				_files = Directory.GetFiles(path);
+			}
+
+			if (files != null)
+			{
+				files = (files.Concat(_files)).ToArray();
+			}
+
+			if (_dirs.Count() == 0)
+			{
+				return;
+			}
+
+			if (directories != null)
+			{
+				directories = (directories.Concat(_dirs)).ToArray();
+			}
+			for (int i = 0; i < _dirs.Count(); i++)
+			{
+				GetAllDirectoryElements(_dirs[i], ref directories, ref files);
+			}
+		}
+	}
+}
