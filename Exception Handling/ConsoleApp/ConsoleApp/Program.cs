603a34cd17a3892fc29b8a7fe621a34f720e54b7
Yury Samaradau <Yury_Samaradau@epam.com>
2018-10-25 20:03:09 +0300

    Exception Handling: +Task 1

---
+++ Exception Handling/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var quit = false;
+
+			while (!quit)
+			{
+				var str = Console.ReadLine();
+				if (str != "quit")
+				{
+					if (!String.IsNullOrWhiteSpace(str))
+					{
+						Console.WriteLine(str[0]);
+					}
+				}
+				else
+				{
+					quit = true;
+				}
+			}
+		}
+	}
+}
